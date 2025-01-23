53fffa1456a19244ff03db2fda184dff59354dff
Pedro Araújo <127028443+pedroara22@users.noreply.github.com>
2025-01-23 18:33:24 +0000

    Last commit

--- src/components/bodies/circleBody.jsx
+++
@@ -1,9 +0,0 @@
-import { Bodies } from "matter-js";
-
-export default class CircleBody {
-    constructor(pos, radius, isStatic) {
-        this.body = Bodies.circle(pos.x + radius, pos.y + radius, radius, { isStatic: isStatic });
-
-        return this.body;
-    }
-}
